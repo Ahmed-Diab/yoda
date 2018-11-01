e2bb69cf0768fed821470900a21f61104c0e9183
Ahmed-Diab <ahhmedeiddiab@gmail.com>
2018-11-01 13:15:36 +0200

    changes on front file

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './sharedComponent/error/error.component';
+import { LoginComponent } from './component/login/login.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+
+  {path:'**', component:ErrorComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routingComponant = [
+
+
+
+]
